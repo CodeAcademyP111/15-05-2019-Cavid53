0a5eecf6ca8b937a6e3cff884e60b86d790335ca
cavid53 <JavidJb@code.edu.az>
2019-05-15 19:18:22 +0400

    05.15.19 homework

---
+++ 0515.19 homework JS/JS/script.js
@@ -0,0 +1,17 @@
+
+function slideShow(){
+
+    document.querySelector(".left-nav").style.marginLeft="0px";
+    document.querySelector(".left-nav").style.transition="1s";
+    
+    
+}
+function closeSlide(){
+    document.querySelector(".left-nav").style.marginLeft="-200px";
+}
+
+
+
+
+
+
